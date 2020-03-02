8297a8c1f544b97cb2340c06dc8b02fbbb79ebff
kurmachu <flyingkurbypikmin@gmail.com>
2020-03-02 11:19:09 -0800

    Initial commit

---
+++ Assets/TelephoneSpinner.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelephoneSpinner : MonoBehaviour
+{
+    public Rigidbody telephoneBody;
+
+    public float dragHeld = 0.05f;
+    private float drag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        drag = telephoneBody.angularDrag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = Vector3.zero;
+        if (Input.touchCount == 1) {
+            Touch touch = Input.GetTouch(0);
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit, 10f, LayerMask.GetMask("Spinorb")))
+            {
+                position = hit.point;
+            }
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit, 10f, LayerMask.GetMask("Spinorb")))
+            {
+                position = hit.point;
+            }
+        }
+
+        if (position != Vector3.zero)
+        {
+            if (!isHeld)
+            {
+                telephoneBody.angularDrag = dragHeld;
+                //telephoneBody.isKinematic = true;
+                isHeld = true;
+                initial = Quaternion.LookRotation(new Vector3(-position.x, -position.y, -position.z), Vector3.up);
+                //initialangle = telephoneBody.rotation;
+
+            }
+            lastangle = telephoneBody.rotation;
+            //telephoneBody.AddTorque(new Vector3(position.y,-position.x,0), ForceMode.VelocityChange);
+            Quaternion change = Quaternion.Euler(Quaternion.LookRotation(new Vector3(-position.x, -position.y, -position.z), Vector3.up).eulerAngles - initial.eulerAngles);
+            //telephoneBody.rotation = change * initialangle;
+            telephoneBody.AddTorque(-change.eulerAngles*Time.deltaTime,ForceMode.Acceleration);
+
+            initial = Quaternion.LookRotation(new Vector3(-position.x, -position.y, -position.z), Vector3.up);
+        }
+        else
+        {
+            if (isHeld)
+            {
+                isHeld = false;
+                telephoneBody.angularDrag = drag;
+
+
+                //Quaternion deltaRotation = telephoneBody.rotation * Quaternion.Inverse(lastangle);
+
+                //deltaRotation.ToAngleAxis(out var angle, out var axis);
+
+                //angle *= Mathf.Deg2Rad;
+
+                //telephoneBody.angularVelocity = (1.0f / Time.deltaTime) * angle * axis;
+
+                //telephoneBody.isKinematic = false;
+            }
+        }
+    }
+
+    private bool isHeld;
+    private Quaternion initial = Quaternion.Euler(0, 0, 0);
+    private Quaternion initialangle = Quaternion.Euler(0, 0, 0);
+    private Quaternion lastangle = Quaternion.Euler(0, 0, 0);
+}
---
+++ Assets/TelephoneSpinnerNew.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelephoneSpinnerNew : MonoBehaviour
+{
+    public Rigidbody telephoneBody;
+
+    public float dragHeld = 0.05f;
+    private float drag;
+    public float maxDrag = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        drag = telephoneBody.angularDrag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool held = false;
+        Vector3 position = Vector3.zero;
+        if (Input.touchCount == 1) {
+            Touch touch = Input.GetTouch(0);
+            position = touch.position;
+            held = true;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            position = Input.mousePosition;
+            held = true;
+        }
+
+        if (held)
+        {
+            if (!lastHeld)
+            {
+                lastHeld = true;
+                lastpos = position;
+            }
+            telephoneBody.angularDrag = Mathf.Lerp(dragHeld, drag, Vector3.Distance(lastpos, position)/maxDrag);
+            Debug.Log(telephoneBody.angularDrag);
+            Vector3 toad = position - lastpos;
+            telephoneBody.AddTorque(new Vector3(toad.y, -toad.x, 0), ForceMode.Acceleration);
+            lastpos = position;
+        }
+        else
+        {
+            telephoneBody.angularDrag = drag;
+            lastHeld = false;
+        }
+    }
+
+    private Vector3 lastpos;
+    private bool lastHeld;
+}
